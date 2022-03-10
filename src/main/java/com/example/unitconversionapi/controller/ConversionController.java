50285a989bdf46fedd047ce2a8a9dc2f69dcd2a5
StrassenDude <simon.kronberger@gmail.com>
2022-03-10 16:15:40 +0100

    Clean up von ConversionController, ConversionService, Response

--- src/main/java/com/example/unitconversionapi/controller/ConversionController.java
+++ src/main/java/com/example/unitconversionapi/controller/ConversionController.java
@@ -1,6 +1,25 @@
 package com.example.unitconversionapi.controller;
 
+import com.example.unitconversionapi.conversion.ConversionService;
+import com.example.unitconversionapi.dto.Request;
+import com.example.unitconversionapi.dto.Response;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import javax.validation.Valid;
+
+@RestController
 public class ConversionController {
 
+    private ConversionService conversionService;
 
+    public ConversionController(ConversionService conversionService) {
+        this.conversionService = conversionService;
+    }
+
+    @PostMapping("/convert")
+    public Response convert(@RequestBody @Valid Request request) {
+        return new Response(request, conversionService.convert(request));
+    }
 }
+
