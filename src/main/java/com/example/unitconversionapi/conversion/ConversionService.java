7750a6d6848cffef0a7d5cde28d974f2096f47e5
jakobCan <jakob.can.code@gmail.com>
2022-03-10 15:11:55 +0100

    implements ConversionService, ConversionStrategy

---
+++ src/main/java/com/example/unitconversionapi/conversion/ConversionService.java
@@ -0,0 +1,29 @@
+package com.example.unitconversionapi.conversion;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Service
+public class ConversionService {
+
+    private final Map<String, ConversionStrategy> conversionStrategyMap;
+
+    /*
+     * Die Liste, die als Argument im Konstruktor ist, sammelt automatisch alle Strategien, die das Interface
+     * ConversionStrategy implementieren.
+     * Diese werden dann mit ihrem jeweiligen ConversionCode als Key in die conversionStrategyMap geschrieben.
+     * Function.identity() ist in diesem Fall ein fancy 'this', also die jeweilige Strategy.
+     * */
+    public ConversionService(List<ConversionStrategy> conversionStrategies) {
+        conversionStrategyMap = conversionStrategies.stream()
+                .collect(Collectors.toMap(ConversionStrategy::getConversionCode, Function.identity()));
+    }
+
+    public float convert(Request request){
+        return 0.0f;
+    }
+}
